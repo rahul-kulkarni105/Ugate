1b7276df8c5aed2fea5ef81463f65c3ba210fc28
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-01-31 11:49:44 -0600

    added imports from sidNav component

---
+++ app/components/dashboard_children/SideNav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import AddPaymentMethod from './grandchildren/AddPaymentMethod'
+import Deposit from './grandchildren/'
+import History from './grandchildren/History'
+import LogOut from './grandchildren/LogOut'
+import SendToRipple from './grandchildren/SendToRipple'
+import Upload from './grandchildren/Upload'
+import Withdraw from './grandchildren/Withdraw'
+
+
+export default class SideNav extends React.Component{
+  render(){
+    return(
+
+    );
+  }
+}
