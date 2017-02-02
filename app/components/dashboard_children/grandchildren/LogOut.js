96c2dbaeb7066899265ba2ce35406e5d57f5e5dc
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-02-02 09:21:17 -0600

    minor

--- app/components/dashboard_children/grandchildren/LogOut.js
+++ app/components/dashboard_children/grandchildren/LogOut.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default class LogOut extends React.Component{
-  render(){
-    return(
+export default class LogOut extends React.Component {
+    handleClick() {
+        // api to set user signed in to false
 
-    );
-  }
+    }
+    render() {
+        return (
+            <div className="row">
+                <button type="button" class="btn btn-danger" onClick={this.handleClick}>
+                    <Link to="/">Logout</Link>
+                </button>
+            </div>
+
+        );
+    }
 }
