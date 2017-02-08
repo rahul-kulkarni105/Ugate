44b78150383ecabf6cf5c58b0006d6ac3a8202f8
michaeledwards91 <michael.edwards1991@gmail.com>
2017-02-08 16:23:34 -0600

    Commented out email-verification stuff. Trying to setup email verification without using package.

--- serverChildren/apiroutes.js
+++ serverChildren/apiroutes.js
@@ -2,7 +2,7 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var User = require("./../models/User");
 var mongoose = require('mongoose');
-var nev = require('email-verification')(mongoose);
+// var nev = require('email-verification')(mongoose);
 
 //nodemailer setup
 var nodemailer = require('nodemailer');
@@ -18,36 +18,38 @@ var auth = {
  
 var transport = nodemailer.createTransport(mg(auth));
 
+//TRYING TO SETUP EMAIL VERIFICATION WITHOUT nev
 //Email verification config
-nev.configure({
-    verificationURL: 'http://localhost:3030/api/users/verify/${URL}',
-    persistentUserModel: User,
-    tempUserCollection: 'tempusers',
+// nev.configure({
+//     verificationURL: 'http://localhost:3030/api/users/verify/${URL}',
+//     persistentUserModel: User,
+//     tempUserCollection: 'tempusers',
  
-    transportOptions: mg(auth),
-    //  {
-    //     service: 'Gmail',
-    //     auth: {
-    //         user: 'myawesomeemail@gmail.com',
-    //         pass: 'mysupersecretpassword'
-    //     }
-    // },
-    verifyMailOptions: {
-        from: 'ugatedonotreply@gmail.com',
-        to: 'ugatedonotreply@gmail.com',
-        subject: 'Please confirm account',
-        html: 'Click the following link to confirm your account:</p><p>${URL}</p>',
-        text: 'Please confirm your account by clicking the following link: ${URL}'
-    }
-}, function(error, options){
-    if(error) {
-        throw error;
-    }
-    console.log(options);
-});
+//     transportOptions: mg(auth),
+//     //  {
+//     //     service: 'Gmail',
+//     //     auth: {
+//     //         user: 'myawesomeemail@gmail.com',
+//     //         pass: 'mysupersecretpassword'
+//     //     }
+//     // },
+//     verifyMailOptions: {
+//         from: 'ugatedonotreply@gmail.com',
+//         to: 'ugatedonotreply@gmail.com',
+//         subject: 'Please confirm account',
+//         html: 'Click the following link to confirm your account:</p><p>${URL}</p>',
+//         text: 'Please confirm your account by clicking the following link: ${URL}'
+//     }
+// }, function(error, options){
+//     if(error) {
+//         throw error;
+//     }
+//     console.log(options);
+// });
 
+//TRYING TO SETUP EMAIL VERIFICATION WITHOUT nev
 // generating the model, pass the User model defined earlier 
-nev.generateTempUserModel(User);
+// nev.generateTempUserModel(User);
 
 module.exports = function(app) {
 
@@ -65,51 +67,54 @@ module.exports = function(app) {
         User.findById(req.params.id, function(err,doc){
             if (err) throw err;
             res.json(doc);
-          })
+          });
     });
 
     //create user and trigger email verification
     app.post("/api/users/new", function(req,res){
         var newUser = new User({username:req.body.username, password:req.body.password, email:req.body.email, phonenumber:req.body.phonenumber, ripplepublicaddress:req.body.ripplepublicaddress, verified:false});
-          // newUser.save(function(err,doc){
-          //   if (err) throw err;
-          //   //email verification steps here?
-            
 
-          //   res.send({doc});
-          // });
-        nev.createTempUser(newUser, function(err, existingPersistentUser, newTempUser) {
-            // some sort of error 
-            if (err) {
-                // handle error... 
-                throw err;
-            }
-         
-            // user already exists in persistent collection... 
-            if (existingPersistentUser) {
-                // handle user's existence... violently. 
-                res.send("User already exists in persistent collection");
-            }
-         
-            // a new user 
-            if (newTempUser) {
-                var URL = newTempUser[nev.options.URLFieldName];
-                nev.sendVerificationEmail(email, URL, function(err, info) {
-                    if (err) {
-                        // handle error... 
-                        throw err;
-                    }
-         
-                    // flash message of success 
-                    res.send("Successfully sent verification email");
-                });
-         
-            // user already exists in temporary collection... 
-            } else {
-                // flash message of failure... 
-                res.send("User already exists in temp collection");
-            }
+        newUser.save(function(err,doc){
+            if (err) throw err;
+            //email verification steps here?
+
+
+            res.send({doc});
         });
+
+        //TRYING TO SETUP EMAIL VERIFICATION WITHOUT NEV
+        // nev.createTempUser(newUser, function(err, existingPersistentUser, newTempUser) {
+        //     // some sort of error 
+        //     if (err) {
+        //         // handle error... 
+        //         throw err;
+        //     }
+         
+        //     // user already exists in persistent collection... 
+        //     if (existingPersistentUser) {
+        //         // handle user's existence... violently. 
+        //         res.send("User already exists in persistent collection");
+        //     }
+         
+        //     // a new user 
+        //     if (newTempUser) {
+        //         var URL = newTempUser[nev.options.URLFieldName];
+        //         nev.sendVerificationEmail(email, URL, function(err, info) {
+        //             if (err) {
+        //                 // handle error... 
+        //                 throw err;
+        //             }
+         
+        //             // flash message of success 
+        //             res.send("Successfully sent verification email");
+        //         });
+         
+        //     // user already exists in temporary collection... 
+        //     } else {
+        //         // flash message of failure... 
+        //         res.send("User already exists in temp collection");
+        //     }
+        // });
 
     });
 
@@ -118,21 +123,22 @@ module.exports = function(app) {
         var url = req.params.url;
         console.log(url);
 
-        nev.confirmTempUser(url, function(err, user) {
-            if (err)
-                // handle error... 
+        //TRYING TO SETUP EMAIL VERIFICATION WITHOUT nev
+        // nev.confirmTempUser(url, function(err, user) {
+        //     if (err)
+        //         // handle error... 
          
-            // user was found! 
-            if (user) {
-                // optional 
-                nev.sendConfirmationEmail(user['email_field_name'], function(err, info) {
-                    // redirect to their profile... 
-                });
-            }
+        //     // user was found! 
+        //     if (user) {
+        //         // optional 
+        //         nev.sendConfirmationEmail(user['email_field_name'], function(err, info) {
+        //             // redirect to their profile... 
+        //         });
+        //     }
          
-            // user's data probably expired... 
-            else
-                // redirect to sign-up 
-        });
+        //     // user's data probably expired... 
+        //     else
+        //         // redirect to sign-up 
+        // });
     });
 }
