edefe4de0decec725a60d75417fe524c060094b0
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-02-16 14:20:42 -0600

    credit card payments should be working through test API in stripe, will test once I have the router up and running

--- app/components/dashboard_children/grandchildren/paymentChildren/Cc.js
+++ app/components/dashboard_children/grandchildren/paymentChildren/Cc.js
@@ -8,10 +8,51 @@ export default class Cc extends React.Component {
     // Add an instance of the card Element into the `card-element` <div>
     card.mount('#card-element');
 
-  }.
+  },
+  componentDidMount(){
+    card.addEventListener('change', function(event) {
+      var displayError = document.getElementById('payment-errors');
+      if (event.error) {
+        displayError.textContent = event.error.message;
+      }
+    });
+
+  },
+  stripeTokenHandler(token) {
+  // Insert the token ID into the form so it gets submitted to the server
+  var form = document.getElementById('payment-form');
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  // Submit the form
+  form.submit();
+},
+  handleSubmit(event) {
+    // Create a token or display an error the form is submitted.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error
+      var errorElement = document.getElementById('payment-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  },
     render() {
         return (
-          <form action="/charge" method="post" id="payment-form">
+          <form action="/charge" method="post" id="payment-form" onSubmit={this.handleSubmit}>
 <div className="form-row">
  <label>
     <span>Card</span>
@@ -22,7 +63,7 @@ export default class Cc extends React.Component {
 </div>
 
 <!-- Used to display form errors -->
-<div id="payment-errors"></div>
+<div id="payment-errors" ></div>
 
 <input type="submit" className="submit" value="Submit Payment" />
 </form>
