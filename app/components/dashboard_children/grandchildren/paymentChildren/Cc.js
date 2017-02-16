0bc37b51ac50a76af0c81e857c357287e078b7f9
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-02-16 12:44:09 -0600

    added some stray code I forgot to push

--- app/components/dashboard_children/grandchildren/paymentChildren/Cc.js
+++ app/components/dashboard_children/grandchildren/paymentChildren/Cc.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 export default class Cc extends React.Component {
+  componentWillMount(){
+    // Create an instance of the card Element
+    var card = elements.create('card');
+
+    // Add an instance of the card Element into the `card-element` <div>
+    card.mount('#card-element');
+
+  }.
     render() {
         return (
           <form action="/charge" method="post" id="payment-form">
