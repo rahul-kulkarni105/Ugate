5973e44fde9a7ec734fa31015c40915044e9e0e9
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-02-20 13:41:31 -0600

    bcrypt has working for user sign up

--- models/User.js
+++ models/User.js
@@ -1,84 +1,102 @@
 // Require mongoose
 var mongoose = require("mongoose");
+var bcrypt = require('bcrypt');
 
+//bcrypt rounds
+const saltRounds = 10;
 // Create a Schema class with mongoose
 var Schema = mongoose.Schema;
 
 // Create a UserSchema with the Schema class
 var UserSchema = new Schema({
-  username: {
-  	type: String,
-  	required: true,
-  	unique: true
+    username: {
+        type: String,
+        required: true,
+        unique: true
 
-  	// validate: [
-  	// function(input){
-  	// 	return input.length >= 6
-  	// },
-  	// "Username must be 6 or more characters"]
-  },
-  password: {
-  	type: String,
-  	required: true
-  },
-  email: {
-  	type: String,
-  	required: true,
-    // match: [/.+\@.+\..+/, "Please enter a valid e-mail address"]
-  },
-  phoneNumber: {
-  	type: String,
-  	required: true,
-  	validate: [
-  	function(input){
-      console.log(input.length);
-  		return input.length === 10
-  	},
-  	"Please enter a valid phone number"]
-  },
-  verificationString: {
-    type: String
-  },
-  verified: {
-    type: Boolean,
-    required: true
-  },
-  //account id
-  ripplePublicAddress: {
-  	type: String,
-  	// required: true,
-  	unique: true
-  },
-  //default current date, maybe use date as string and use momentjs?
-  accountCreation: {
-    type: Date,
-    default: Date.now
-  },
-  history: {
-  	currentBalance: [Number],
-  	deposits: [Number],
-  	withdraws: [Number],
-  	sentToRipple: [Number],
-  	depositsFromRipple: [Number],
-    //date maybe a string and use momentjs?
-    lastModified: [Date]
-  },
-  usid: {
-  	front: {
-      data: Buffer,
-  		type: String,
-  		required: true
-  	},
-  	back: {
-      data: Buffer,
-  		type: String,
-  		required: true
-  	}
-  },
-  lock: {
-  	type: Boolean,
-  	default: false
-  }
+        // validate: [
+        // function(input){
+        // 	return input.length >= 6
+        // },
+        // "Username must be 6 or more characters"]
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        // match: [/.+\@.+\..+/, "Please enter a valid e-mail address"]
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        validate: [
+            function(input) {
+                console.log(input.length);
+                return input.length === 10
+            },
+            "Please enter a valid phone number"
+        ]
+    },
+    verificationString: {
+        type: String
+    },
+    verified: {
+        type: Boolean,
+        required: true
+    },
+    //account id
+    ripplePublicAddress: {
+        type: String,
+        // required: true,
+        unique: true
+    },
+    //default current date, maybe use date as string and use momentjs?
+    accountCreation: {
+        type: Date,
+        default: Date.now
+    },
+    history: {
+        currentBalance: [Number],
+        deposits: [Number],
+        withdraws: [Number],
+        sentToRipple: [Number],
+        depositsFromRipple: [Number],
+        //date maybe a string and use momentjs?
+        lastModified: [Date]
+    },
+    // usid: {
+    //     front: {
+    //         data: Buffer,
+    //         type: String,
+    //         required: true
+    //     },
+    //     back: {
+    //         data: Buffer,
+    //         type: String,
+    //         required: true
+    //     }
+    // },
+    lock: {
+        type: Boolean,
+        default: false
+    }
+});
+
+//hook for password hashing
+UserSchema.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('password'))
+        return next();
+    bcrypt.hash(user.password, saltRounds, function(err, hash) {
+        if (err)
+            return next(err);
+        user.password = hash;
+        next();
+    });
+
 });
 
 // Create the User model with the UserSchema
