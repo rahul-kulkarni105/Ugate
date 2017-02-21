a81d8dfd7baed3ce2d83944a22b503e47101d404
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-02-21 00:51:38 -0600

    did some fornt end work while I was to tired to think about back end, it will be finished by demo day

--- app/components/dashboard_children/grandchildren/Upload.js
+++ app/components/dashboard_children/grandchildren/Upload.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Upload extends React.Component{
   render(){
     return(
-      <form method="post" enctype="multipart/form-data" action="/upload">
+      <form method="post" encType="multipart/form-data" action="/upload">
           <input type="file" name="file" />
           <input type="submit" value="Submit" />
       </form>
