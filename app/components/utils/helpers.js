fe69b7ca743f19b7a98eea262134746eccbf3065
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-02-20 19:28:28 -0600

    login for jwt token

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-
-
+//funciton to get history api for user, not finished
 export function getHistory() {
     return axios.get("/history");
 }
 
+//creates user in MonogoDB
 export function createUser(userInfo) {
     return axios.post("/api/users/new", {
 
@@ -24,5 +24,18 @@ export function createUser(userInfo) {
         console.log(response);
     }).catch(function(error) {
         console.log(error);
+    });
+}
+
+//login helper function
+export function login(userInfo) {
+    return axios.post('/api/authenticate', {
+        email: userInfo.email,
+        password: userInfo.password
+    }).then(function(response) {
+        console.log(response);
+    }).catch(function(error) {
+        console.log(error);
+
     });
 }
