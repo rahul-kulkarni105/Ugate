ba4fa6a4fd25b3407f98e4c22c73dff5070bab1b
june.k.lara@gmail.com <june.k.lara@gmail.com>
2017-02-22 17:04:56 -0600

    spaced out words in side nav

--- app/components/dashboard_children/SideNav.js
+++ app/components/dashboard_children/SideNav.js
@@ -15,14 +15,14 @@ export default class SideNav extends React.Component {
                     </a>
                 </li>
                 {/* logo goes here */}
-                <li><Link to="/AddPaymentMethod">AddPaymentMethod</Link></li>
+                <li><Link to="/AddPaymentMethod">Add Payment Method</Link></li>
                 <li><Link to="/Deposit">Deposit</Link></li>
                 <li><Link to="/History">History</Link></li>
-                <li><Link to="/SendToRipple">SendToRipple</Link></li>
+                <li><Link to="/SendToRipple">Send To Ripple</Link></li>
                 <li><Link to="/Upload">Upload</Link></li>
                 <li><Link to="/Withdraw">Withdraw</Link></li>
                 <li><Link to="/RippleCharts">Ripple Charts</Link></li>
-                <li><Link to="/LogOut">LogOut</Link></li>
+                <li><Link to="/LogOut">Log Out</Link></li>
               </ul>
             </div>
 
